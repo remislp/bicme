e9c0db7bf648c0f7537fc077ab69d08080d04d8a
remislp <r.lape@ucl.ac.uk>
2017-01-20 11:25:50 +0000

    Starting Python implementation.

---
+++ bicme/__init__.py
@@ -0,0 +1,10 @@
+"""
+bicme
+=======
+
+Bayesian analysis of single-channel currents with exact correction for missed events.
+This is a Python implementation of bicme package originally written by Michael Epstein.
+"""
+
+from bicme import *
+
---
+++ bicme/tests/test_MH_sampler.py
@@ -0,0 +1,8 @@
+from bicme.samplers.Metropolis_Hastings import MH
+
+sampler = MH()
+sampler.block_sample()
+sampler.component_sample()
+
+
+
