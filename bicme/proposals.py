41b11c56276448c1cfe65b59088e399c8c79d686
remislp <r.lape@ucl.ac.uk>
2017-02-23 22:02:04 +0000

    log block proposal added.

--- bicme/proposals.py
+++ bicme/proposals.py
@@ -24,11 +24,27 @@ class RWMHProposal():
         if (not isinf(logLik)) and (not isnan(logLik)):
             alpha = min(0, logLik - logLik0)
         return alpha, proposal, logLik
-
+    
+    def __get_alpha_logpars(self, proposal, logLik0):
+        try:
+            logLik = self.model(proposal, self.data)
+        except:
+            logLik = float('nan')
+        alpha = -float('inf')
+        if (not isinf(logLik)) and (not isnan(logLik)):
+            alpha = min(0, (logLik + np.sum(np.log(proposal)) - 
+                           (logLik0 + np.sum(np.log(theta)))))
+        return alpha, proposal, logLik
+    
     def propose_block(self, theta, logLik0, scale, ip=None):
         L = np.linalg.cholesky(np.identity(len(theta)) * scale)
         proposal = theta.copy() +  np.dot(L.T, np.random.normal(size=len(theta)))
         return self.__get_alpha(proposal, logLik0)
+    
+    def propose_block_log(self, theta, logLik0, scale, ip=None):
+        L = np.linalg.cholesky(np.identity(len(theta)) * scale)
+        proposal = np.log(theta.copy()) +  np.dot(L.T, np.random.normal(size=len(theta)))
+        return self.__get_alpha_logpars(np.exp(proposal), logLik0)
     
     def propose_component(self, theta, logLik0,  scale, ip):
         proposal = theta.copy()
@@ -38,12 +54,7 @@ class RWMHProposal():
     def propose_component_log(self, theta, logLik0, scale, ip):
         proposal = theta.copy()
         proposal[ip] = exp(random.normalvariate(log(theta[ip]), scale[ip]))
-        logLik = self.model(proposal, self.data)
-        alpha = -float('inf')
-        if not np.isinf(logLik):
-            alpha = min(0, (logLik + np.sum(np.log(proposal)) - 
-                           (logLik0 + np.sum(np.log(theta)))))
-        return alpha, proposal, logLik
+        return self.__get_alpha_logpars(np.exp(proposal), logLik0)
         
     def propose_block_mixture(self, theta, logLik0, mass_matrix, ip=None, need_mixture=True):
         """
@@ -54,25 +65,9 @@ class RWMHProposal():
         k = len(theta)
         if need_mixture:
             L = np.linalg.cholesky((pow(2.38, 2) / k) * mass_matrix)
-            mix1 = (1 - beta) * (theta + np.dot(L.T, np.random.normal(size=k)))
-            mix2 = beta * (theta + (0.1 / sqrt(k)) * np.random.normal(size=k))
-            proposal = mix1 + mix2
+            proposal = (1 - beta) * (theta + np.dot(L.T, np.random.normal(size=k)))
+            proposal += beta * (theta + (0.1 / sqrt(k)) * np.random.normal(size=k))
         else:
             proposal = theta + (0.1 / sqrt(k)) * np.random.normal(size=k)                
         return self.__get_alpha(proposal, logLik0)
                            
-    def propose_component_mixture(self, theta, logLik0, mass_matrix, ip, need_mixture):
-        """
-        Propose a joint Metropolis-Hastings step for the parameters of a
-        model based on proposing from a mixture distribution.
-        """
-        beta = 0.05
-        k = len(theta)
-        if need_mixture:
-            #L = np.linalg.cholesky((pow(2.38, 2) / k) * mass_matrix)
-            mix1 = (1 - beta) * (theta[ip] + np.dot(mass_matrix, np.random.normal(size=k))[ip])
-            mix2 = beta * (theta[ip] + (0.1 / sqrt(k)) * np.random.normal(size=1))
-            proposal = mix1 + mix2
-        else:
-            proposal = theta[ip] + (0.1 / sqrt(k)) * np.random.normal(size=1)
-        return self.__get_alpha(proposal, logLik0)
--- bicme/run_TestSampler.py
+++ bicme/run_TestSampler.py
@@ -4,6 +4,7 @@ import scipy.io as sio
 import scipy.stats
 
 from samplers import MWGSampler
+from samplers import RosenthalAdaptiveSampler
 from proposals import RWMHProposal
 from display import quick_display
 
@@ -11,53 +12,51 @@ def norm_logPosterior(X, y):
     return norm_logLik(X, y) + norm_logPrior(X)
 
 def norm_logLik(X, y):
-    return -0.5 * (len(y) * math.log(2 * math.pi * math.pow(X[1], 2)) +
-            (1 / math.pow(X[1], 2)) * np.sum(np.power(y - X[0], 2)))
-    #return np.sum(np.log(scipy.stats.norm.pdf(y, X[0], X[1]))) 
+    #return -0.5 * (len(y) * math.log(2 * math.pi * math.pow(X[1], 2)) +
+    #        (1 / math.pow(X[1], 2)) * np.sum(np.power(y - X[0], 2)))
+    return np.sum(np.log(scipy.stats.norm.pdf(y, X[0], X[1]))) 
     
-
-
 def norm_logPrior(X):
     mu_lims = [-20, 20]
     si_lims = [0, 100]
-    return (np.log(scipy.stats.uniform.pdf(X[0], mu_lims[0], mu_lims[1])) 
+    return (np.log(scipy.stats.uniform.pdf(X[0], mu_lims[0], 40)) 
               + np.log(scipy.stats.uniform.pdf(X[1], si_lims[0], si_lims[1])))
 
 def test_normal_case_MWG(data):
-    
     X0 = [2, 10]
     # Initialise Proposer
     proposer = RWMHProposal(norm_logPosterior, data, verbose=True)
-
     # Initialise Sampler
-    sampler = MWGSampler(samples_draw=100000, notify_every=1000, 
+    sampler = MWGSampler(samples_draw=10, notify_every=1, 
+                         burnin_fraction=0.5, burnin_lag=5,
+                         model=norm_logPosterior, data=data, 
+                         proposal=proposer.propose_block,
+                         verbose=True)  
+    sampler.acceptance_limits = [0.3, 0.7]
+    sampler.scale = 0.5
+    # Sample
+    S = sampler.sample_block(X0)
+    print('acceptances= ', sampler.acceptances)
+    print('proposals= ', sampler.proposals)
+    print('samples= ', sampler.samples)
+    #quick_display(S, burnin=5)
+    
+def test_normal_case_RosenthalAdaptive(data):
+    # Initialise Proposer
+    proposer = RWMHProposal(norm_logPosterior, data, verbose=True)
+    # Initialise Sampler
+    sampler = RosenthalAdaptiveSampler(samples_draw=100000, notify_every=100, 
                          burnin_fraction=0.5, burnin_lag=50,
                          model=norm_logPosterior, data=data, 
-                         proposal=proposer.propose_component,
+                         proposal=proposer.propose_block_mixture,
                          verbose=True)                         
     # Sample
-    S = sampler.sample_component(X0)
-
-    #print('acceptances= ', sampler.acceptances)
-    #print('proposals= ', sampler.all_proposals)
-    #print('samples= ', sampler.all_samples)
-
+    X0 = [2, 10]
+    S = sampler.sample_block(X0)
     quick_display(S, burnin=5000)
 
 mat1 = sio.loadmat('../Tests/Data/NormData.mat')
-#for key, value in mat1.items() :
-#    print (key)
 data = np.array(mat1['data']).flatten()
-#print('data: ', data)
-
-X0 = [1.3509862348087593, 11.1811660419655325]
-print('X0= ', X0)
-print('logLik= ', norm_logPosterior(X0, data))
-X1 = [-0.1950727276889210, 10.4227127446818404]
-print('X1= ', X1)
-print('logLik= ', norm_logPosterior(X1, data))
-
-
-#data = np.array([-1.97794885e+01, 1.04568872e+01, 5.14080369e+00, -1.21177720e+00])
-
-#test_normal_case_MWG(data)
+np.random.seed(1)
+test_normal_case_MWG(data)
+#test_normal_case_RosenthalAdaptive(data)
