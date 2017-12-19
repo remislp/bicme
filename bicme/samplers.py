2065bfff479770c65a2d0389b5b6ec7301fd440f
remislp <r.lape@ucl.ac.uk>
2017-12-19 14:23:52 +0000

    Refactoring: introduced sampling chain structure.

--- bicme/samplers.py
+++ bicme/samplers.py
@@ -2,6 +2,38 @@ from math import log
 from random import random
 import numpy as np
 
+
+class Chain(object):
+    """Data structure to save MCMC sampling result."""
+    def __init__(self, k, N, sample_type='block'):
+        self.stype = sample_type
+        self.proposals = np.zeros((k, N))
+        self.samples = np.zeros((k, N))
+        if self.stype == 'block':
+            self.acceptances = np.zeros(N)
+            self.posteriors = np.zeros(N)
+            self.scale_factors = np.zeros(N)
+        elif self.stype == 'component':
+            self.acceptances = np.zeros((k, N))
+            self.posteriors = np.zeros((k, N))
+            self.scale_factors = np.zeros((k, N))
+            
+    def insert_sample_block(self, i, thetaP, theta0, L0, scale, acceptance):
+        self.proposals[:, i] = thetaP
+        self.scale_factors[i] = scale
+        self.samples[:, i] = theta0
+        self.posteriors[i] = L0
+        self.acceptances[i] = acceptance
+
+    def insert_sample_component(self, i, j, 
+                                thetaP, theta0, L0, scale, acceptance):
+        self.proposals[j, i] = thetaP
+        self.scale_factors[j, i] = scale
+        self.samples[j, i] = theta0
+        self.posteriors[j, i] = L0
+        self.acceptances[j, i] = acceptance
+
+
 class Sampler(object):
     """
     Random-walk sampling algorithm with fixed jump size. 
@@ -35,15 +67,10 @@ class Sampler(object):
         self.B = int(samples_draw * burnin_fraction)
         self.lag = burnin_lag
         self.model = model
-        #self.data = data
         self.proposal = proposal
         self.verbose = verbose
         self.acceptance_limits = [0.1, 0.5]
         self.scale = 0.1
-        self.scale_factors = []
-        self.proposals = []
-        self.samples = []
-        self.acceptances = np.zeros(self.N)
         print('Sampler initialised...')
         
     def do_print(self, i):
@@ -54,24 +81,27 @@ class Sampler(object):
         """
         theta - starting values for the parameters
         """
-        S = np.zeros((len(theta) + 1, self.N))
-        ll0 = self.model(theta) #, self.data)
-        local_theta = theta.copy()
+        chain = Chain(len(theta), self.N)
+        L0 = self.model(theta) #, self.data)
+        theta0 = theta.copy()
         for i in range(self.N):
+            is_accepted = 0
             # Generate candidate state
-            p = self.proposal(local_theta)
+            thetaP = self.proposal(theta0)
              # accept / reject new state
-            if p.all() > 0:
+            if thetaP.all() > 0:
                 try:
-                    llp = self.model(p) #, self.data)
+                    Lp = self.model(thetaP) #, self.data)
                 except:
-                    llp = float('inf')
-                alpha = min(1, np.exp(llp-ll0))
+                    Lp = float('inf')
+                alpha = min(1, np.exp(Lp-L0))
                 if random() < alpha:
-                    local_theta, ll0 = p, llp
-            S[ : , i] = np.append(local_theta, ll0)
+                    theta0, L0 = thetaP, Lp
+                    is_accepted = 1
+            
+            chain.insert_sample(i, thetaP, theta0, L0, 1, is_accepted)
             self.do_print(i+1)
-        return S
+        return chain
     
     def tune_scale(self, scale_factor, acceptance_proportion, i):
         message = 'not changed'
@@ -98,63 +128,54 @@ class MWGSampler(Sampler):
                  
     def sample_block(self, theta):
         self.k = len(theta)
+        chain = Chain(self.k, self.N, 'block')
         scale_factor = 1
-        L0 = self.model(theta) #, self.data)
+        L0 = self.model(theta)
         theta0 = theta.copy()
         for i in range(self.N):
-            # Get proposal
-            alpha, theta_p, Lp = self.proposal(theta0, L0, scale_factor, None)
-            self.proposals.append(theta_p)
-            # Check alpha
+            is_accepted = 0
+            # Get proposal and check alpha
+            alpha, thetaP, Lp = self.proposal(theta0, L0, scale_factor, None)
             if alpha == 0 or alpha > log(random()):
-                self.acceptances[i] = 1
-                theta0, L0 = theta_p, Lp
-            self.samples.append(np.append(theta0, L0))
-            self.scale_factors.append(scale_factor)
-            #if (i+1) % self.lag == 0:
+                is_accepted = 1
+                theta0, L0 = thetaP, Lp
+            chain.insert_sample_block(i, 
+                thetaP, theta0, L0, scale_factor, is_accepted)
+            # Tune sampler step
             if ((i+1) % self.lag == 0) and (i < self.B):
                 acceptance_proportion = (np.sum(
-                    self.acceptances[i - self.lag + 1 : i + 1]) / self.lag)
+                    chain.acceptances[i - self.lag + 1 : i + 1]) / self.lag)
                 scale_factor = self.tune_scale(scale_factor, acceptance_proportion, i)
             self.do_print(i+1)
-        return np.array(self.samples).T
+        return chain
         
     def sample_component(self, theta):
         """
         Samples parameters one by one to improwe mixing.
         """
         self.k = len(theta)
-        self.acceptances = np.zeros((self.k, self.N))
-        self.proposals = np.zeros((self.k, self.N))
-        self.samples = np.zeros((self.k, self.N))
-        self.posteriors = np.zeros((self.k, self.N))
+        chain = Chain(self.k, self.N, 'component')
         scale_factor = np.ones(self.k)
-        L0 = self.model(theta) #, self.data)
+        L0 = self.model(theta)
         theta0 = theta.copy()
         # Sampling loop
         for i in range(self.N):
             # Sample parameter one at a time
             for j in range(self.k):
-                self.posteriors[j, i] = L0
-                self.samples[j, i] = theta0[j]
-                alpha, theta_p, Lp = self.proposal(theta0, L0, scale_factor, j)
-                self.proposals[j, i] = theta_p[j] #.append(theta_p)
+                is_accepted = 0
+                alpha, thetaP, Lp = self.proposal(theta0, L0, scale_factor, j)
                 if alpha == 0 or alpha > log(random()):
-                    self.acceptances[j , i] = 1
-                    theta0, L0 = theta_p, Lp
-                    self.samples[j, i] = theta_p[j]
-                    self.posteriors[j, i] = Lp
-                    
-                #self.samples.append(np.append(theta0, L0))
+                    is_accepted = 1
+                    theta0, L0 = thetaP, Lp
+                chain.insert_sample_component(i, j, 
+                    thetaP[j], theta0[j], L0, scale_factor[j], is_accepted)
                 # Tune sampler step
-                self.scale_factors.append(scale_factor)
                 if ((i+1) % self.lag == 0) and (i < self.B):
                     acceptance_proportion = (np.sum(
-                        self.acceptances[j, i - self.lag + 1 : i + 1]) / (self.lag))
+                        chain.acceptances[j, i - self.lag + 1 : i + 1]) / (self.lag))
                     scale_factor[j] = self.tune_scale(scale_factor[j], acceptance_proportion, i)
-            #self.samples.append(np.append(theta0, L0))
             self.do_print(i+1)  
-        return self.samples, self.posteriors, self.proposals
+        return chain
 
     
 class RosenthalAdaptiveSampler(Sampler):
@@ -173,22 +194,19 @@ class RosenthalAdaptiveSampler(Sampler):
         Samples parameters one by one to improwe mixing.
         """         
         self.k = len(theta)
+        chain = Chain(self.k, self.N, 'block')
         start_adaption = 2 * self.k
         need_mixture = False
         mass_matrix = np.identity(self.k)
         scale_factor = 1
         L0 = self.model(theta) #, self.data)
         theta0 = theta.copy()
-        
-        #self.posteriors = []
-        self.proposals = np.zeros((self.N, self.k))
-        self.samples = np.zeros((self.N, self.k))
-        self.posteriors = np.zeros((1, self.N))
-        
+               
         #mass_matrix = np.identity(self.k) * scale_factor
         
         # Sampling loop
         for i in range(self.N):
+            is_accepted = 0
             # Update covariance matrix
             if i+1 >= start_adaption:
                 need_mixture = True
@@ -196,18 +214,17 @@ class RosenthalAdaptiveSampler(Sampler):
             else:
                 mass_matrix = np.identity(self.k) * scale_factor
             # Get proposal
-            alpha, theta_p, Lp = self.proposal(theta0, L0, mass_matrix, need_mixture)
-            self.proposals[i] = theta_p
+            alpha, thetaP, Lp = self.proposal(theta0, L0, mass_matrix, need_mixture)
             # Check alpha
             if alpha == 0 or alpha > log(random()):
-                self.acceptances[i] = 1
-                theta0, L0 = theta_p, Lp
-            self.samples[i] = theta0
-            self.posteriors[:, i] = L0
-            self.scale_factors.append(scale_factor)
+                is_accepted = 1
+                theta0, L0 = thetaP, Lp
+            chain.insert_sample_block(i, 
+                thetaP, theta0, L0, scale_factor, is_accepted)
+            
             if ((i+1) % self.lag == 0) and (i < self.B):
                 acceptance_proportion = (np.sum(
                     self.acceptances[i - self.lag + 1 : i + 1]) / self.lag)
                 scale_factor = self.tune_scale(scale_factor, acceptance_proportion, i)
             self.do_print(i+1)
-        return self.samples.T, self.posteriors, self.proposals.T
+        return chain
