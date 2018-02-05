651f02903c4502e35e7c4ad554d5fc8ef253f2fd
remislp <r.lape@ucl.ac.uk>
2018-02-05 07:56:18 +0000

    Higher level functions for MCMC sampling added to bicme.helpers

--- bicme/helpers.py
+++ bicme/helpers.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python
 
 import math
+import time
 import numpy as np
 import scipy.stats
 import scipy.io as sio
+from scipy.optimize import minimize
+
+from bicme.samplers import MWGSampler
+from bicme.samplers import RosenthalAdaptiveSampler
+from bicme.proposals import RWMHProposal
 
 try:
     from HJCFIT.likelihood import Log10Likelihood
@@ -43,6 +49,47 @@ def load_bursts_from_matfile(fmat):
             rec.append(arr[0].tolist())
         bursts.append(rec)
     return bursts, conc, tr, tc
+
+def sample_MWG(log_posterior, theta, N=100000, sample_comp=True):
+    """
+    Run Metropolis_within_Gibbs (MWG) sampling component- or block-wise.
+    """
+    proposer = RWMHProposal(log_posterior, verbose=True)
+    if sample_comp:
+        print('Sampling componentwise...')
+        proposal=proposer.propose_component_log
+    else:
+        print('Sampling block...')
+        proposal=proposer.propose_block_log
+    sampler = MWGSampler(samples_draw=N, notify_every=int(N/100), 
+                             burnin_fraction=0.5, burnin_lag=50,
+                             model=log_posterior, #data=args, 
+                             proposal=proposal, 
+                             verbose=True) 
+    print ("\nInitial posterior = {0:.6f}".format(log_posterior(theta)))
+    start = time.clock()
+    if sample_comp:
+        chain = sampler.sample_component(theta)
+    else:
+        chain = sampler.sample_block(theta)
+    print ('\nCPU time in MWG sampler =', time.clock() - start)
+    return chain
+
+def sample_RA(log_posterior, theta, N=100000):
+    """
+    Run Rosenthal adaptive sampling with mixture proposal.
+    """
+    proposer = RWMHProposal(log_posterior, verbose=True)
+    sampler = RosenthalAdaptiveSampler(samples_draw=N, notify_every=int(N/100), 
+                         burnin_fraction=0.5, burnin_lag=50,
+                         model=log_posterior, #data=args, 
+                         proposal=proposer.propose_block_mixture,
+                         verbose=True)
+    print ("\nInitial posterior = {0:.6f}".format(log_posterior(theta)))
+    start = time.clock()
+    chain = sampler.sample_block(theta)
+    print ('\nCPU time in Rosenthal sampler =', time.clock() - start)
+    return chain
 
 
 class CaseHJCFIT(object):
@@ -104,3 +151,30 @@ class CaseHJCFIT(object):
             likelihood.append(Log10Likelihood(self.bursts[i], self.mec.kA,
                 self.tr[i], self.tc[i], **self.kwargs))
         return likelihood
+    
+    def run_MLL_fit(self, initial_guess=None):
+        """
+        Run HJCFIT: maximum log likelihood fit.
+        Parameters:"""
+    
+        if initial_guess is not None:
+            theta = initial_guess
+        else: 
+            theta = self.mec.theta()
+        self.logSpace = True
+        print("\nRunning MLL fit; Starting likelihood (DCprogs)= {0:.6f}".
+                format(self.logLik(np.log(theta))))
+        start = time.clock()
+        success = False
+        while not success:
+            result = minimize(self.logLik, np.log(theta), 
+                method='Nelder-Mead', #callback=printiter,
+                options={'xtol':1e-5, 'ftol':1e-5, 
+                'maxiter': 30000, 'maxfev': 150000, 'disp': True})
+            success = result.success
+            self.mec.theta_unsqueeze(np.exp(result.x))
+        print ('CPU time in simplex=', time.clock() - start)
+        print ("\nDCPROGS Fitting finished: %4d/%02d/%02d %02d:%02d:%02d"
+                %time.localtime()[0:6])
+        print ("\n Final rate constants:", self.mec)
+
